f0fc98fda531c9c91df6bccc474be6cfc1bb9ee8
tsuyoshiwada <v23.b.wada@gmail.com>
2017-08-18 23:36:36 +0900

    oops

--- Button.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import Button from '../src/Button';
-
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button type="button">Primary Button</Button>
-  ));
